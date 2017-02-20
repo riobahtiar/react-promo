5a8c59b2a135eac656291d551e31cdef572189b8
afifkhaidir <afif.khaidir@gmail.com>
2017-02-20 09:49:28 +0700

    Create footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -14,18 +14,60 @@ import Link from '../Link';
 
 class Footer extends React.Component {
   render() {
+    const date = new Date();
+
     return (
       <div className={s.root}>
         <div className={s.container}>
-          <span className={s.text}>© Your Company</span>
-          <span className={s.spacer}>·</span>
-          <Link className={s.link} to="/">Home</Link>
-          <span className={s.spacer}>·</span>
-          <Link className={s.link} to="/admin">Admin</Link>
-          <span className={s.spacer}>·</span>
-          <Link className={s.link} to="/privacy">Privacy</Link>
-          <span className={s.spacer}>·</span>
-          <Link className={s.link} to="/not-found">Not Found</Link>
+          <div className={s.footerCopyright}>
+            &copy; 2009-{date.getFullYear()}, PT Tokopedia
+          </div>
+          <div className={s.footerConnect}>
+            <ul className={s.footerList}>
+              <li className={s.footerList__item}>
+                <Link to="https://itunes.apple.com/us/app/tokopedia/id1001394201?ls=1&mt=8">
+                  <img className={s.footerConnect__image} src="https://ecs7.tokopedia.net/assets/images/store/appstore.png" alt="appstore" />
+                </Link>
+              </li>
+              <li className={s.footerConnectList__item}>
+                <Link to="https://play.google.com/store/apps/details?id=com.tokopedia.tkpd">
+                  <img className={s.footerConnect__image} src="https://ecs7.tokopedia.net/assets/images/store/googleplay.png" alt="playstore" />
+                </Link>
+              </li>
+            </ul>
+            <ul className={s.footerConnectList}>
+              <li className={s.footerConnectList__item}>
+                <Link to="https://www.facebook.com/tokopedia">
+                  <img className={s.footerConnect__image} src="https://ecs7.tokopedia.net/assets/images/social/facebook.png" alt="facebook" />
+                </Link>
+              </li>
+              <li className={s.footerConnectList__item}>
+                <Link to="http://instagram.com/tokopedia">
+                  <img className={s.footerConnect__image} src="https://ecs7.tokopedia.net/assets/images/social/instagram.png" alt="instagram" />
+                </Link>
+              </li>
+              <li className={s.footerConnectList__item}>
+                <Link to="https://twitter.com/tokopedia">
+                  <img className={s.footerConnect__image} src="https://ecs7.tokopedia.net/assets/images/social/twitter.png" alt="twitter" />
+                </Link>
+              </li>
+              <li className={s.footerConnectList__item}>
+                <Link to="https://www.youtube.com/user/tokopedia">
+                  <img className={s.footerConnect__image} src="https://ecs7.tokopedia.net/assets/images/social/youtube.png" alt="youtube" />
+                </Link>
+              </li>
+              <li className={s.footerConnectList__item}>
+                <Link to="https://plus.google.com/+tokopedia/posts">
+                  <img className={s.footerConnect__image} src="https://ecs7.tokopedia.net/assets/images/social/googleplus.png" alt="google_plus" />
+                </Link>
+              </li>
+              <li className={s.footerConnectList__item}>
+                <Link to="https://blog.tokopedia.com/">
+                  <img className={s.footerConnect__image} src="https://ecs7.tokopedia.net/assets/images/social/blog.png" alt="blog_tokopedia" />
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     );
